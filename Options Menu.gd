f8335310930ad390c1fc5eb3d1b774257760abe0
codeWonderland <alice.easterdevelops@gmail.com>
2023-10-05 15:58:32 -0400

    File organization and scene navigation

---
+++ Options Menu.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_save_button_pressed():
+	# TODO: Save data
+	_on_cancel_button_pressed()
+	
+	
+func _on_cancel_button_pressed():
+	get_tree().change_scene_to_file("res://Main Menu.tscn")
