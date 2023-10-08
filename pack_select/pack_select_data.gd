4a25511927efe39a938c5355e90f3052e31b23ba
Petra Andraschko <51335299+monochromia@users.noreply.github.com>
2023-10-07 22:46:19 -0500

    Bugfixes, image loading now works

--- pack_select/pack_select_data.gd
+++ pack_select/pack_select_data.gd
@@ -24,19 +24,21 @@ func _ready():
 
 
 func load_image_into_texture(image_path):
-	var image = Image.new()  # Create a new Image instance
-	var error = image.load(image_path)  # Load the image file
+	var im = Image.new()
+	im = Image.load_from_file(image_path)  # Create a new Image instance
 	
-	if error == OK:  # Check if the image loaded successfully
+	#TODO: Set pack size dynamically based on number of rows/columns decided by screen size or user settings? idk.
+	# This solution works but messes up resolution something fierce
+	im.resize(150,225)
+	
+	if im != Image.new():  # Check if the image loaded successfully
 		var texture = ImageTexture.new()  # Create a new ImageTexture instance
-		texture.create_from_image(image)  # Assign the image to the texture
+		
+		texture = ImageTexture.create_from_image(im)  # Assign the image to the texture
 		return texture  # Return the texture
 	else:
 		print("Failed to load image: ", image_path)
 		return null
-		
-		
-
 
 func load_cards():
 	var card_backs_dir = DirAccess.open(mod_folder_path + "/Card Backs App")
