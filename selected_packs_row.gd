42222b521fb64e6c0c534b6d1f84435691aba091
codeWonderland <alice.easterdevelops@gmail.com>
2023-10-20 17:54:32 -0400

    Added num drafts screen

--- selected_packs_row.gd
+++ selected_packs_row.gd
@@ -3,6 +3,11 @@ extends PanelContainer
 @onready var active_packs_row = $MarginContainer/ActivePacksRow
 
 const Pack = preload("res://pack_select/selected_pack.tscn")
+@export var active_packs: Array[PackData] = []
+
+
+func get_packs() -> Array[PackData]: 
+	return active_packs
 
 
 func set_pack_select_data(pack_select_data: PackSelectData):
@@ -14,6 +19,7 @@ func on_pack_clicked(pack: PackData, left_click: bool):
 		var selected_pack = Pack.instantiate()
 		active_packs_row.add_child(selected_pack)
 		selected_pack.set_pack_data(pack)
+		active_packs.append(pack)
 	else:
 		var children = active_packs_row.get_children()
 		
@@ -21,3 +27,8 @@ func on_pack_clicked(pack: PackData, left_click: bool):
 			if child and child.pack.name == pack.name:
 				active_packs_row.remove_child(child)
 				break
+				
+		# Remove only one instance of the pack from active_packs array
+		var index_to_remove = active_packs.find(pack)
+		if index_to_remove != -1:
+			active_packs.remove_at(index_to_remove)
