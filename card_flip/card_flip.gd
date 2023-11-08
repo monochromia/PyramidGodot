1cbdb1cdef6b3a0552d02b3ae84c299639336d19
codeWonderland <alice.easterdevelops@gmail.com>
2023-11-08 10:15:01 -0500

    Adjusting card flip scene

--- card_flip/card_flip.gd
+++ card_flip/card_flip.gd
@@ -1,11 +1,33 @@
-extends Node2D
+extends PanelContainer
 
+signal redraft_card(index: int)
+
+var is_revealed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_card_click(event: InputEvent) -> void:
+	print("on card click")
+	if event is InputEventMouseButton \
+			and event.button_index == MOUSE_BUTTON_LEFT \
+			and event.is_pressed() \
+			and not is_revealed:
+		flip_card()
+		is_revealed = true
+		
+	elif event is InputEventMouseButton \
+			and event.button_index == MOUSE_BUTTON_RIGHT \
+			and event.is_pressed() \
+			and is_revealed:
+		redraft_card.emit(get_index())
+
+func flip_card():
+	print("flipping card")
+	$AnimationPlayer.play("card_flip")
